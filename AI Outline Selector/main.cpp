9f50aaf3e6815d070d2471afd6e17afe5496cb93
PixelGM <amosprayogo@gmail.com>
2023-06-29 21:51:17 -0700

    Trackbars to adjust the HSV

--- AI Outline Selector/main.cpp
+++ AI Outline Selector/main.cpp
@@ -1,39 +1,46 @@
 #include <opencv2/opencv.hpp>
 
 int main() {
-    // Load the image file
-    cv::Mat img = cv::imread("WhatsApp Image 2023-06-22 at 8.34.55 PM.jpeg");
+    cv::namedWindow("Control", cv::WINDOW_NORMAL); // Create window for control sliders
 
-    // Check if image was loaded successfully
-    if (img.empty()) {
-        std::cout << "Could not read the image file" << std::endl;
-        return 1;
+    int iLowH = 0;
+    int iHighH = 10;
+
+    int iLowS = 70;
+    int iHighS = 255;
+
+    int iLowV = 50;
+    int iHighV = 255;
+
+    // Create trackbars in "Control" window
+    cv::createTrackbar("LowH", "Control", &iLowH, 179); // Hue (0 - 179)
+    cv::createTrackbar("HighH", "Control", &iHighH, 179);
+
+    cv::createTrackbar("LowS", "Control", &iLowS, 255); // Saturation (0 - 255)
+    cv::createTrackbar("HighS", "Control", &iHighS, 255);
+
+    cv::createTrackbar("LowV", "Control", &iLowV, 255); // Value (0 - 255)
+    cv::createTrackbar("HighV", "Control", &iHighV, 255);
+
+    while (true)
+    {
+        cv::Mat img = cv::imread("WhatsApp Image 2023-06-22 at 8.34.55 PM.jpeg"); // Read the image
+        cv::Mat imgHSV;
+        cv::cvtColor(img, imgHSV, cv::COLOR_BGR2HSV); // Convert the captured frame from BGR to HSV
+
+        cv::Mat imgThresholded;
+        cv::inRange(imgHSV, cv::Scalar(iLowH, iLowS, iLowV), cv::Scalar(iHighH, iHighS, iHighV), imgThresholded); // Threshold the image
+
+        // Show the original image and the thresholded image
+        cv::imshow("Original", img);
+        cv::imshow("Thresholded Image", imgThresholded);
+
+        // Wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+        if (cv::waitKey(30) == 27) {
+            std::cout << "esc key is pressed by user" << std::endl;
+            break;
+        }
     }
-
-    // Convert color space from BGR to HSV
-    cv::Mat hsv;
-    cv::cvtColor(img, hsv, cv::COLOR_BGR2HSV);
-
-    // Define range for red color
-    cv::Mat mask;
-    cv::inRange(hsv, cv::Scalar(0, 70, 50), cv::Scalar(10, 255, 255), mask);
-
-    // Find contours in the mask
-    std::vector<std::vector<cv::Point>> contours;
-    cv::findContours(mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-
-    // Draw rectangles around each contour
-    for (const auto& contour : contours) {
-        cv::Rect rect = cv::boundingRect(contour);
-        cv::rectangle(img, rect, cv::Scalar(0, 255, 0), 2);
-    }
-
-    // Display the image
-    cv::namedWindow("Image with Rectangle", cv::WINDOW_NORMAL);
-    cv::imshow("Image with Rectangle", img);
-
-    // Wait for a keystroke in the window
-    cv::waitKey(0);
 
     return 0;
 }
